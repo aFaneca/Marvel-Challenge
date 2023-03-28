a31cfb34c2a30468ec23e1865f721224a66406c8
António Faneca <me@afaneca.com>
2023-03-28 21:22:16 +0100

    added AppDispatchers, Constants & Resource utility classes

---
+++ app/src/main/java/com/afaneca/marvelchallenge/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.afaneca.marvelchallenge.common
+
+object Constants {
+    const val API_BASE_URL = "https://gateway.marvel.com:443"
+}
--- app/src/main/java/com/afaneca/marvelchallenge/di/AppModule.kt
+++ app/src/main/java/com/afaneca/marvelchallenge/di/AppModule.kt
@@ -1,8 +1,12 @@
 package com.afaneca.marvelchallenge.di
 
+import com.afaneca.marvelchallenge.common.AppDispatchers
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -23,6 +27,8 @@ object AppModule {
     //endregion
 
     //region misc
-
+    @Provides
+    @Singleton
+    fun provideAppDispatchers(): AppDispatchers = AppDispatchers(Dispatchers.IO)
     //endregion
 }
